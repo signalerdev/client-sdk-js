e77657dc4c7f932bc69ecd28ac9445f9a8c62188
Lukas Herman <lherman.cs@gmail.com>
2024-11-16 17:52:43 -0500

    update esbuild

--- demo-react/bundle.ts
+++ demo-react/bundle.ts
@@ -1,20 +1,22 @@
-import * as esbuild from "npm:esbuild@0.20.2";
+import * as esbuild from "npm:esbuild@0.24.0";
 // Import the Wasm build on platforms where running subprocesses is not
 // permitted, such as Deno Deploy, or when running without `--allow-run`.
 // import * as esbuild from "https://deno.land/x/esbuild@0.20.2/wasm.js";
 
 import { denoPlugins } from "jsr:@luca/esbuild-deno-loader@^0.11.0";
 
+// Safari ESBuild undefined: https://github.com/evanw/esbuild/issues/3952
 await esbuild.build({
   plugins: [...denoPlugins()],
   entryPoints: ["./index.tsx"],
   outfile: "./dist/index.js",
   bundle: true,
-  minify: false,
-  treeShaking: false,
+  minify: true,
+  treeShaking: true,
   sourcemap: "inline",
   write: true,
-  format: "esm",
+  format: "iife",
+  platform: "browser",
   target: "chrome109 edge128 firefox115 ios15.6 opera112 safari15.6".split(" "),
 });
 
