bc2a5c0a931bfa8da4eed06450abc8165f35d305
Lukas Herman <lherman.cs@gmail.com>
2024-11-20 20:19:24 -0500

    fix: github workflow for e2e and deploy

--- demo-react/src/main.tsx
+++ demo-react/src/main.tsx
@@ -1,13 +1,17 @@
 import { createRoot } from 'react-dom/client'
+// @ts-ignore: no type
 import GetUserMediaMock from "@theopenweb/get-user-media-mock";
 import App from './App.tsx'
 
 const urlParams = new URLSearchParams(window.location.search);
+// @ts-ignore: no type
 window.getUserMediaMock = new GetUserMediaMock();
 if (urlParams.get("mock") !== null) {
+  // @ts-ignore: no type
   window.getUserMediaMock.mock();
   console.log("force mock");
 } else {
+  // @ts-ignore: no type
   window.getUserMediaMock.fallbackMock();
   console.log("fallback mock");
 }
